230ade80d3fd8a063e2a305f569ac2e5a35bff05
CBManuelCSP <54774260+CBManuelCSP@users.noreply.github.com>
2025-11-20 17:09:12 +0100

    Modificación script1.py

--- script1.py
+++ script1.py
@@ -8,5 +8,5 @@ area = longitud * anchura
 print("El área del rectángulo es: ", area)
 
 # Agora con un círculo
-radio = float(input("Introduce el radio del círculo"))
+radio = float(input("Introduce el radio del círculo (Modificación web)"))
 print("El área del círculo es ", radio*radio*3.14)
