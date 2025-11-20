90110d95713dbb80e0da8fc265616f9ee9581637
CBManuelCSP <nosequeponermewww@gmail.com>
2025-11-20 13:52:52 +0100

    Tarefa 3.1

---
+++ script1.py
@@ -0,0 +1,8 @@
+longitud_str = input("Introduce la longitud: ")
+anchura_str = input("Introduce la anchura: ")
+
+longitud = float(longitud_str)
+anchura = float(anchura_str)
+
+area = longitud * anchura
+print("El área del rectángulo es: ", area)
