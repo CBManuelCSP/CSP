7cb344b2d99eb090b01883d5102255f73139d6fa
CBManuelCSP <nosequeponermewww@gmail.com>
2025-12-11 17:20:45 +0100

    Jupyter

--- script1.py
+++ script1.py
@@ -6,3 +6,7 @@ anchura = float(anchura_str)
 
 area = longitud * anchura
 print("El área del rectángulo es: ", area)
+
+# Agora con un círculo
+radio = float(input("Introduce el radio del círculo"))
+print("El área del círculo es ", radio*radio*3.14)
